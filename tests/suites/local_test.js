10a219d33bbcd3bbb7dee0e411d7db886e6e31f9
Abhi Singh <abhi.s@browserstack.com>
2021-12-22 00:19:44 +0530

    removes pause command

--- tests/suites/local_test.js
+++ tests/suites/local_test.js
@@ -9,29 +9,19 @@ describe("Website Tests", () => {
 
   it("screenshots", async (browser) => {
     await browser.waitForElementVisible(".wrapper");
-    await browser.pause(5000);
     await saveScreenshot(browser, "HomePage");
-    // browser.saveScreenshot('images/index.png');
 
     await browser.url(`${browser.launchUrl}/login.html`);
-    await browser.pause(2000);
     await saveScreenshot(browser, "Login");
-    // browser.saveScreenshot('images/login.png');
 
     await browser.url(`${browser.launchUrl}/signup.html`);
-    await browser.pause(2000);
     await saveScreenshot(browser, "Signup");
-    // browser.saveScreenshot('images/signup.png');
 
     await browser.url(`${browser.launchUrl}/404_error.html`);
-    await browser.pause(2000);
     await saveScreenshot(browser, "404_error");
-    // browser.saveScreenshot('images/404_error.png');
 
     await browser.url(`${browser.launchUrl}/500_error.html`);
-    await browser.pause(2000);
     await saveScreenshot(browser, "500_error");
-    // browser.saveScreenshot('images/500_error.png');
   });
 
   after(commonHooks.after);
